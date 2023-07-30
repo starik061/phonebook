783c4b954ca90beeb0605d76384955dfec309ea0
starik061 <starik061@gmail.com>
2023-07-30 10:49:18 +0300

    add phone mask

--- src/components/AddContactForm/AddContactForm.jsx
+++ src/components/AddContactForm/AddContactForm.jsx
@@ -1,7 +1,95 @@
 import React from 'react';
+import InputMask from 'react-input-mask';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { logIn } from 'redux/auth/operations';
+
+const defaultTheme = createTheme();
 
 const AddContactForm = () => {
-  return <div>AddContactForm</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: '5px',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <PersonAddIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            New contact
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={console.log('Submit')}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="name"
+              label="Contact name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+            />
+            <InputMask
+              mask="+38 (099) 999-99-99"
+              // value="99/99/9999"
+              // onChange={}
+            >
+              {inputProps => (
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="phone"
+                  label="Phone number"
+                  type="text"
+                  id="phone"
+                />
+              )}
+            </InputMask>
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Add contact
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2"></Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default AddContactForm;
